1b6af45fd9f59cb6c0d26e81665ec6897db5e2ba
muhammadharoun <hro19502001@gmail.com>
2022-07-07 15:47:55 +0300

    finsh the script

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-
-
+import json
+import re
+import pandas as pd
 class CourseraScraper:
     ROOT = 'https://www.coursera.org'
 
@@ -33,26 +34,41 @@ class CourseraScraper:
 
         
     def get_html(self, url): 
-        return requests.get(url)
+        return requests.get(url,headers=self.headers).text
 
-    def parse_courses(self):
-        return  {            
-        'Name':'',
-        'Url':'',
-        'Rating':'',
-        'Tags':'',
-        'Description':''
-        }
+    def parse_courses(self,html):
+        result = []
+        soup = BeautifulSoup(html, 'html.parser')
+        mydivs = soup.find("body").find("script").text.split('{"context"')[1].split(',"plugins":{}};')[0][1::]
+        json_object = json.loads(mydivs)
+        courses = json_object['dispatcher']['stores']['AlgoliaResultsStateStore']['resultsState'][2]['content']['hits']
+        for obj in courses:
+            name = obj['name']
+            url = self.ROOT + obj['objectUrl']
+            tag = ', '.join(obj['skills']) 
+            Rating = round(float(obj['avgProductRating']), 1)
+            description = obj['_snippetResult']['description']['value']
+            div = {'Name':name,'Url':url,'Rating':Rating,'Tags':tag ,'Description':description}
+            result.append(div)
+        
+        return result
 
-    def handle_scrap(self):
-        return
+    def handle_scrap(self,fromPage,toPage):
+        for i in range(fromPage, toPage):
+            htmlContent = self.get_html(self.ROOT+f'/search?page={i}&index=prod_all_launched_products_term_optimization')
+            try:
+                Courses = self.parse_courses(htmlContent)
+                self.save_csv(Courses)
+            except:
+                pass
 
-    def save_csv(self):
-        with open('coursera.csv', 'a') as f:
-            writer = csv.DictWriter(f, self.column_names)
-            writer.writerow()
+    def save_csv(self,arr):
+        df = pd.DataFrame(arr)
+        df.to_csv('coursera.csv', mode='a',index=False, header=False)
 
 
 
 if __name__ == '__main__':
+    scraper = CourseraScraper()
+    scraper.handle_scrap(1,84)
     print('__name__')
